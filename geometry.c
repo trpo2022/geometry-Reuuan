f4e9a93b93907ddfc5a1410d6bdcc42bb9730a46
Revin <revin2003@yandex.ru>
2022-02-25 23:18:29 +0700

    Added data entry validation in geometry.c

--- geometry.c
+++ geometry.c
@@ -5,31 +5,73 @@
 
 int main()
 {
-char string[m];
-int b = 0, n = 0;
-printf("Enter the name of the shape and parameters\n");
+    char string[m];
+    int b = 0, n = 0, br1, br2;
+    printf("Enter the name of the shape and parameters\n");
 
-fgets(string, m, stdin);
+    fgets(string, m, stdin);
+    for (int i = 0; i<m; i++)
+    {
+        if (string[i] =='(')
+        {
+            b =1;
+            br1 = i;
+            break;
+        }
+    }
+    if(b == 0)
+    {
+        printf("Incorrect data. Expecdet '('\n");
 
-for (int i = 0; string[i] != '('; i++)
-{
-n++;
-}
+    }
+    b = 0;
+    for (int i = 0; i<m; i++)
+    {
+        if (string[i] ==')')
+        {
+            b =1;
+            br2 = i;
+            break;
+        }
+    }
+    if(b == 0)
+    {
+        printf("Incorrect data. Expecdet ')'\n");
+    }
+    if(br1>br2)
+    {
+        printf("Incorrect data\n");
+    }
+    b = 0;
+    for (int i = 0; i<m; i++)
+    {
+        if (string[i] ==',')
+        {
+            b =1;
 
-char name[n];
-for (int i = 0; string[i] != '('; i++)
-{
-name[i] = string[i];
-}
-int i = 0;
-if ((strcmp(name, "circle") != 0) && (strcmp(name, "triangle") != 0))
-{
-printf("Incorrect shape name\n");
-}
-else
-{
-printf("Ok\n");
-}
+            break;
+        }
+    }
+    if(b == 0)
+    {
+        printf("Incorrect data. Expected ','");
+    }
+    for (int i = 0; string[i] != '('; i++)
+    {
+        n++;
+    }
 
-return 0;
+
+    char name[n];
+    for (int i = 0; string[i] != '('; i++)
+    {
+        name[i] = string[i];
+    }
+    int i = 0;
+    if ((strcmp(name, "circle") != 0) && (strcmp(name, "triangle") != 0))
+    {
+        printf("Incorrect shape name\n");
+    }
+
+    return 0;
 }
