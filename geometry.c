07b9e6d1f81633d84156bef6a31fcfac8616c3ab
Revin <revin2003@yandex.ru>
2022-02-27 16:45:55 +0700

    Redacted geometry.c(code formatted and compilation error fixed)

--- geometry.c
+++ geometry.c
@@ -10,66 +10,51 @@ int main()
     printf("Enter the name of the shape and parameters\n");
 
     fgets(string, m, stdin);
-    for (int i = 0; i<m; i++)
-    {
-        if (string[i] =='(')
-        {
-            b =1;
+    for (int i = 0; i < m; i++) {
+        if (string[i] == '(') {
+            b = 1;
             br1 = i;
             break;
         }
     }
-    if(b == 0)
-    {
+    if (b == 0) {
         printf("Incorrect data. Expecdet '('\n");
-
     }
     b = 0;
-    for (int i = 0; i<m; i++)
-    {
-        if (string[i] ==')')
-        {
-            b =1;
+    for (int i = 0; i < m; i++) {
+        if (string[i] == ')') {
+            b = 1;
             br2 = i;
             break;
         }
     }
-    if(b == 0)
-    {
+    if (b == 0) {
         printf("Incorrect data. Expecdet ')'\n");
     }
-    if(br1>br2)
-    {
+    if (br1 > br2) {
         printf("Incorrect data\n");
     }
     b = 0;
-    for (int i = 0; i<m; i++)
-    {
-        if (string[i] ==',')
-        {
-            b =1;
+    for (int i = 0; i < m; i++) {
+        if (string[i] == ',') {
+            b = 1;
 
             break;
         }
     }
-    if(b == 0)
-    {
+    if (b == 0) {
         printf("Incorrect data. Expected ','");
     }
-    for (int i = 0; string[i] != '('; i++)
-    {
+    for (int i = 0; string[i] != '('; i++) {
         n++;
     }
 
-
     char name[n];
-    for (int i = 0; string[i] != '('; i++)
-    {
+    for (int i = 0; string[i] != '('; i++) {
         name[i] = string[i];
     }
-    int i = 0;
-    if ((strcmp(name, "circle") != 0) && (strcmp(name, "triangle") != 0))
-    {
+
+    if ((strcmp(name, "circle") != 0) && (strcmp(name, "triangle") != 0)) {
         printf("Incorrect shape name\n");
     }
 
